74029e32bba710d537d9faf42e0600830f5f26a1
DrissDL <115555391+DrissDL@users.noreply.github.com>
2022-10-27 17:55:28 +0200

    deplacement perso

---
+++ JS/perso.js
@@ -0,0 +1,25 @@
+// function move_right(event) {
+
+//     document.getElementById("perso").style.left = event.clientX + 'px';
+// }
+
+// document.addEventListener("click", move_right);
+let perso = document.getElementById('perso');
+let x = 0;
+
+function move_right(event) {
+    console.log('event.keyCode=' + event.keyCode);
+    if (event.keyCode == 39) {
+        x += 10;
+        perso.style.left = (parseInt(x)) + 'px';
+    }
+    if (event.keyCode == 37) {
+        x -= 10;
+        perso.style.left = (parseInt(x)) + 'px';
+    }
+}
+
+
+document.onkeydown = move_right;
+// document.onkeydown = move_left;
+
