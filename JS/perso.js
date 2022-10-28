2f0ad38e03083069bdaa2142e1819a52735ec119
DrissDL <115555391+DrissDL@users.noreply.github.com>
2022-10-28 16:39:39 +0200

    V1 deplacmement + gravité

--- JS/perso.js
+++ JS/perso.js
@@ -4,31 +4,40 @@
 // }
 
 // document.addEventListener("click", move_right);
+
 let perso = document.getElementById('perso');
 let x = 0;
 let y = 0;
+let ymax = 100;
+
+
+
+function gravity(gravY) {
+    
+}
 
 function move(event) {
     console.log('event.keyCode=' + event.keyCode);
-    if (event.keyCode == 39) {
-        x += 10;
-        perso.style.left = (parseInt(x)) + 'px';
-    }
-
-    if (event.keyCode == 37) {
-        x -= 10;
-        perso.style.left = (parseInt(x)) + 'px';
-    }
-
-    if (event.keyCode == 38) {
-        y += 100;
-        perso.style.bottom = (parseInt(y)) + 'px';
-        
-        while (y != 0) {
-            
-            perso.style.bottom = (parseInt(y)) + 'px';
-            y -=1;
-        }
+    switch (event.keyCode) {
+        case 39:
+            x += 10;
+            perso.style.left = x + 'px';
+            break;
+        case 37:
+            x -= 10;
+            perso.style.left = x + 'px';
+            break;
+        case 38:
+            y += 100;
+            perso.style.bottom = y + 'px';
+            for (let i = 0; i < ymax; i++) {
+                setTimeout(() => {
+                    perso.style.bottom = y + 'px';
+                    y -= 1;
+                    console.log("coucou");
+                }, 2000);
+            }
+            break;
     }
 }
 
