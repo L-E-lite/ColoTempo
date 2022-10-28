a99ec9238189b0db337aeea6814017c6227da9c4
DrissDL <115555391+DrissDL@users.noreply.github.com>
2022-10-28 11:23:09 +0200

    dossier ressources + debut saut

--- JS/perso.js
+++ JS/perso.js
@@ -6,6 +6,7 @@
 // document.addEventListener("click", move_right);
 let perso = document.getElementById('perso');
 let x = 0;
+let y = 0;
 
 function move(event) {
     console.log('event.keyCode=' + event.keyCode);
@@ -13,12 +14,30 @@ function move(event) {
         x += 10;
         perso.style.left = (parseInt(x)) + 'px';
     }
+
     if (event.keyCode == 37) {
         x -= 10;
         perso.style.left = (parseInt(x)) + 'px';
     }
+
+    if (event.keyCode == 38) {
+        y += 100;
+        perso.style.bottom = (parseInt(y)) + 'px';
+        
+        while (y != 0) {
+            
+            perso.style.bottom = (parseInt(y)) + 'px';
+            y -=1;
+        }
+    }
 }
+
 
 
 document.onkeydown = move;
 
+/* V1 Saut "téléporter" 
+Gravité 
+Redescencdre progressivement -> timer
+V2 Saut progressif 
+*/
